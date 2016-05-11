0a13accf5acff77466e492f1684c77a984efc144
Josh Samuelson <js@puppetlabs.com>
2016-05-11 14:12:38 -0700

    Increase proxy timeout

--- manifests/init.pp
+++ manifests/init.pp
@@ -32,14 +32,16 @@ class selfpaced (
 
   include nginx
   nginx::resource::vhost { 'try.puppet.com':
-    ssl_port             => '443',
-    ssl                  => true,
-    ssl_cert             => '/etc/ssl/try.puppet.com.crt',
-    ssl_key              => '/etc/ssl/try.puppet.com.key',
-    use_default_location => false,
-    locations            => {
-      '/sandbox/'        => {
-        proxy            => 'http://127.0.0.1:3000',
+    ssl_port               => '443',
+    ssl                    => true,
+    ssl_cert               => '/etc/ssl/try.puppet.com.crt',
+    ssl_key                => '/etc/ssl/try.puppet.com.key',
+    use_default_location   => false,
+    locations              => {
+      '/sandbox/'          => {
+        proxy_read_timeout => '1h',
+        proxy_connect_timeout => '1h',
+        proxy              => 'http://127.0.0.1:3000',
         proxy_set_header => [
           'Upgrade $http_upgrade',
           'Connection "Upgrade"',
