f981353da1ea79e8ab4f5d168692d2da772e3241
Josh Samuelson <js@puppet.com>
2016-09-19 12:02:41 -0700

    Make sure network is up in container

--- manifests/lxd.pp
+++ manifests/lxd.pp
@@ -3,16 +3,12 @@ class selfpaced::lxd {
   include apt
   apt::ppa { 'ppa:ubuntu-lxc/lxc-stable': }
 
-  package { ['lxd','ruby-dev']:
+  package { ['lxd','ruby-dev','g++']:
     ensure  => present,
     require => Apt::Ppa['ppa:ubuntu-lxc/lxc-stable'],
   }
 
   file { '/var/lib/lxd/files':
     ensure => directory,
-  }
-  file { '/var/lib/lxd/files/bashrc':
-    ensure => file,
-    source => 'puppet:///modules/selfpaced/agent/bashrc',
   }
 }
