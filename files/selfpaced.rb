85a3a471f4a8686b750e1449f3f99db8e05b2bd6
Josh Samuelson <js@puppet.com>
2016-11-15 12:32:22 -0800

    Check the uuid to avoid shell injection
    This adds a check on user provided uuid to make sure that it is a valid
    uuid. This means only alphanumeric characters which prevents shell
    injection.

--- files/selfpaced.rb
+++ files/selfpaced.rb
@@ -79,7 +79,7 @@ end
 options = {}
 OptionParser.new do |opt|
  opt.on('--course COURSENAME') { |o| options[:course_name] = o }
- opt.on('--uuid CONTAINER') { |o| options[:uuid] = o }
+ opt.on('--uuid CONTAINER') { |o| options[:uuid] = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/.match(o) ? o : nil  }
 end.parse!
 
 case options[:course_name]
